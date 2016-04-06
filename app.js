f343c9a5875fc91da4a507e4627f88ee7ed99802
gmiriam <Administrador@AT-SIGA-01-021.ull.local>
2016-04-06 11:53:07 +0100

    Arrancado el proyecto con un ejemplo

---
+++ app.js
@@ -0,0 +1,20 @@
+﻿import 'zone.js/lib/browser/zone-microtask';  
+import 'reflect-metadata';
+
+import { Component, View, bootstrap } from 'angular2/angular2';  
+import { HelloComponent } from './hello.component';
+
+@Component({
+  selector: 'hello-app'
+})
+@View({
+  directives: [HelloComponent],
+  template: `
+    <div>
+      <hello-component></hello-component>
+    </div>
+  `
+})
+class HelloApp { }
+
+bootstrap(HelloApp, []);  
---
+++ hello.component.js
@@ -0,0 +1,13 @@
+﻿import { Component, View } from 'angular2/angular2';
+
+@Component({
+  selector: 'hello-component'
+})
+@View({
+  template: `<p>{{ message }}</p>`
+})
+export class HelloComponent {  
+  constructor() {
+    this.message = "Hola, Angular 2!";
+  }
+}
