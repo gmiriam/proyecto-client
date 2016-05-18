b431b0de33e0986617261897c8e76f4ff5ccefd1
gmiriam <Administrador@AT-SIGA-01-021.ull.local>
2016-05-18 12:19:22 +0100

    Añade funcionalidades de routing

--- src/app/ts/app.component.ts
+++ src/app/ts/app.component.ts
@@ -2,18 +2,36 @@
 import { Component }       from '@angular/core';
 import { HeroService }     from './hero.service';
 import { HeroesComponent } from './heroes.component';
+import { DashboardComponent} from './dashboard.component';
 @Component({
   selector: 'my-app',
-  template: `
-	<h1>{{title}}</h1>
-	<my-heroes></my-heroes>
-  `,
-  directives: [ROUTER_DIRECTIVES],
-  providers: [
-    ROUTER_PROVIDERS,
-    HeroService
-  ]
+	template: `
+	  <h1>{{title}}</h1>
+	  <nav>
+		<a [routerLink]="['Dashboard']">Dashboard</a>
+		<a [routerLink]="['Heroes']">Heroes</a>
+	  </nav>
+	  <router-outlet></router-outlet>
+	`,
+	directives: [ROUTER_DIRECTIVES],
+	providers: [
+	  ROUTER_PROVIDERS,
+	  HeroService
+	]
 })
+@RouteConfig([
+	{
+	  path: '/dashboard',
+	  name: 'Dashboard',
+	  component: DashboardComponent,
+	  useAsDefault: true
+	},		
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  }
+])
 export class AppComponent {
   title = 'Tour of Heroes';
 }
