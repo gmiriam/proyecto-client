069ee3201a5a1c0f991d84fbf2056f8a8d98f969
gmiriam <gmiriam87@gmail.com>
2016-06-19 21:33:58 +0100

    Implementa borrado y añadido de tarea

--- src/app/ts/tasks.ts
+++ src/app/ts/tasks.ts
@@ -1,22 +1,41 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router-deprecated';
-import { CORE_DIRECTIVES, FORM_DIRECTIVES } from '@angular/common';
+import { CORE_DIRECTIVES, FORM_DIRECTIVES, FormBuilder, Validators } from '@angular/common';
 import { Http, Headers, Response } from '@angular/http';
 
 
 @Component({
-  selector: 'tasks',
-  directives: [RouterLink, CORE_DIRECTIVES, FORM_DIRECTIVES ],
+	selector: 'tasks',
+	directives: [RouterLink, CORE_DIRECTIVES, FORM_DIRECTIVES ],
   templateUrl: 'src/app/html/tasks.html',
   //  styleUrls: ['./login.css']
 })
 export class Tasks {
 	taskList: Object[];
+	formEnable: boolean;
+	taskForm: Object;
 
-  constructor(public router: Router, public http: Http) {
-	  this.getTasks();
+	constructor(public router: Router, public http: Http, fb: FormBuilder) {
+		this.getTasks();
+		this.taskForm = fb.group({
+	    	name: ["", Validators.required],
+	    	statement: ["", Validators.required],
+			startDate: ["", Validators.required],
+			endDate: [""],
+			maxScore: [""],
+			teacher: ["", Validators.required]
+		});
+	}
+	showForm(event) {
+	  this.formEnable = true;
   }
-  getTasks() {
+
+  	onSubmit(event) {
+  		console.log("Event: ", event, this.taskForm.value)
+		this.formEnable = false;
+		this.add(this.taskForm.value)
+	}
+	getTasks() {
 	  this.http.get('http://localhost:3000/task/findAll')
 		.subscribe(
           response => {
@@ -32,6 +51,46 @@ export class Tasks {
           	  console.log(response.json())
 		  },
 		  error => {
+			  console.error(error.text());
+		  }
+ 	  );
+  }
+
+	add(task) {
+    let body = JSON.stringify(task);
+	console.debug("mando", body)
+	    let bodyEncoded = Object.keys(task).map(function(k) {
+	      return encodeURIComponent(k) + '=' + encodeURIComponent(task[k])
+	    }).join('&');
+	    let headers = new Headers();
+	    headers.append('Content-Type', 'application/json');
+
+	    this.http.post('http://localhost:3000/task/add', body, { headers: headers })
+	      .subscribe(
+	        response => {
+	          console.log(response)
+				this.getTasks();
+
+	        },
+	        error => {
+	          alert(error.text());
+	          console.log(error.text());
+	        }
+	      );
+	}
+
+  delete(task, event) {
+	this.http.delete('http://localhost:3000/task/delete/' + task._id)
+	  	.subscribe(
+          response => {
+			  var status = response.json().status;
+          	  console.log(status)
+          	  if(status == "success") {
+          	  	alert("Se ha borrado con éxito")
+				this.getTasks();
+			  }
+		},
+		error => {
 			  console.error(error.text());
 		  }
  	  );
