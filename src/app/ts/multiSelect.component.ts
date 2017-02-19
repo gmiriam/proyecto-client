a3da2a266cf7f4680046d87b17f348062c5f787d
gmiriam <gmiriam87@gmail.com>
2017-02-19 14:08:37 +0000

    Añade eventos de selección y borrado a selectores

--- src/app/ts/multiSelect.component.ts
+++ src/app/ts/multiSelect.component.ts
@@ -8,6 +8,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class MultiSelectComponent {
 	@Input() public items:Array<any>;
 	@Output() change: EventEmitter<Array<any>> = new EventEmitter();
+	@Output() select: EventEmitter<Array<any>> = new EventEmitter();
+	@Output() remove: EventEmitter<Array<any>> = new EventEmitter();
 	@Input() private value:any;
 
 	public onChange(value:any):void {
@@ -18,10 +20,20 @@ export class MultiSelectComponent {
 		serializedSelection && this.change.next(serializedSelection);
 	}
 
+	public onSelect(value:any):void {
+
+		this.select.next(value.id);
+	}
+
+	public onRemove(value:any):void {
+
+		this.remove.next(value.id);
+	}
+
 	_getSerializedSelection(selection) {
 
 		if (!selection || !selection.length) {
-			return null;
+			return [];
 		}
 
 		return selection.map(function(currentValue, index, array) {
--- src/app/ts/singleSelect.component.ts
+++ src/app/ts/singleSelect.component.ts
@@ -8,6 +8,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class SingleSelectComponent {
 	@Input() public items:Array<any>;
 	@Output() change: EventEmitter<Array<any>> = new EventEmitter();
+	@Output() select: EventEmitter<Array<any>> = new EventEmitter();
+	@Output() remove: EventEmitter<Array<any>> = new EventEmitter();
 	@Input() private value:any;
 
 	public onChange(value:any):void {
@@ -16,6 +18,16 @@ export class SingleSelectComponent {
 		var serializedSelection = this._getSerializedSelection(value);
 
 		serializedSelection && this.change.next(serializedSelection);
+	}
+
+	public onSelect(value:any):void {
+
+		this.select.next(value.id);
+	}
+
+	public onRemove(value:any):void {
+
+		this.remove.next(value.id);
 	}
 
 	_getSerializedSelection(selection) {
